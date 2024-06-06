5750d648a0564cf06042ae7c7cad3a21dc54fefd
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-06 10:54:15 +0300

    crmdjangoproject

---
+++ crmProject/accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email','date_created')
+admin.site.register(Customer, CustomerAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category','description','date_created')
+admin.site.register(Product, ProductAdmin)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('date_created', 'status')
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Tag)
