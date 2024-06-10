fd9da2994c0530096c6b8ae0037c1dee76097c24
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-10 12:12:09 +0300

    updates

--- crmProject/accounts/urls.py
+++ crmProject/accounts/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>/', views.customer, name='customer'),
-    path('createorder/', views.createOrder, name='createorder'),
+    
+    path('create_order/<str:pk>/', views.createOrder, name='createorder'),
     path('creatcustomer/', views.createCustomer, name='createcustomer'),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 
 ]
