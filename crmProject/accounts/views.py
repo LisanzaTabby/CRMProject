93c67f8edef7b4b879b6a325eeb3c8ca68332f27
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-11 14:37:15 +0300

    initial commit

--- crmProject/accounts/views.py
+++ crmProject/accounts/views.py
@@ -7,6 +7,7 @@ from .filter import OrderFilter
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 @login_required(login_url='loginPage')
 def home(request):
